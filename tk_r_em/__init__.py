9f5c19096761a1749ee563a0b95c1a8b7ec6fb76
Ivanlh20 <ivanlh20@gmail.com>
2023-04-14 14:13:09 +0100

    fix __init__

--- tk_r_em/__init__.py
+++ tk_r_em/__init__.py
@@ -1,8 +1,8 @@
 __name__ = 'tk_r_em'
 __version__ = '1.0.1'
 __description__ = 'Deep convolutional neural networks to restore single-shot electron microscopy images'
-__author__ = 'Ivan Lobato',
-__author_email__='ivanlh20@gmail.com',
+__author__ = 'Ivan Lobato'
+__author_email__='ivanlh20@gmail.com'
 __url__ = 'https://github.com/Ivanlh20/r_em/'
 __credits__ = 'University of Antwerp'
 __license__ = 'GPLv3'
